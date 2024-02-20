9fc6fd0d75e055b929a5d23b115d7951d9c73e97
Joonas Lehtinen <644969+jojule@users.noreply.github.com>
2024-02-20 13:31:05 -0800

    Cleaned the code

--- src/main/java/com/example/application/FlowView.java
+++ src/main/java/com/example/application/FlowView.java
@@ -8,19 +8,23 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("flow")
+@Route("contacts")
 public class FlowView extends VerticalLayout {
 
     public FlowView(ContactService contactService) {
         var grid = new Grid<>(Contact.class);
         grid.setItems(contactService.getContacts());
 
-        grid.addComponentColumn(contact -> new Image(contact.pictureUrl(), contact.name())).setAutoWidth(true);
+        grid.addComponentColumn(contact -> 
+                new Image(contact.pictureUrl(), contact.name())).setAutoWidth(true);
+
         grid.addColumn(Contact::name).setHeader("Name").setAutoWidth(true);
         grid.addColumn(Contact::company).setHeader("Company").setAutoWidth(true);
         grid.addColumn(Contact::jobTitle).setHeader("Job Title").setAutoWidth(true);
-        grid.addComponentColumn(contact -> new DatePicker(contact.lastContacted(), e ->
-                Notification.show("Updated last contacted date for " + contact.name() + " to " + e.getValue())
+
+        grid.addComponentColumn(contact -> 
+                new DatePicker(contact.lastContacted(), e ->
+                    Notification.show(contact.name() + " updated at " + e.getValue())
         )).setHeader("Last Contacted").setAutoWidth(true);
 
         add(new H1("Leads"), grid);
